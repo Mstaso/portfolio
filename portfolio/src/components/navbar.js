b740ab75fb4a2889954f4b3e3d692b1321b2413d
malcolm <mstaso1@gmail.com>
2020-11-30 12:03:52 -0500

    started navbar

---
+++ portfolio/src/components/navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+class Navbar extends React.Component{
+    render() {
+        return(
+            <div id='navbar'>
+               <ul>
+                   <li>Home</li>
+                   <li>Projects</li>
+                   <li>Resume</li>
+                   <li></li>
+
+                   </ul> 
+            </div>
+        )
+    }
+}
+
+export default Navbar;

cc5710012b376b09cb427113230c7a3fbfb5e3c5
malcolm <mstaso1@gmail.com>
2020-12-06 17:35:02 -0500

    more project styling

--- portfolio/src/components/project.js
+++ portfolio/src/components/project.js
@@ -4,16 +4,30 @@ import React from 'react'
 class Project extends React.Component {
     render() {
         console.log(this.props)
-        let tools = this.props.project.tools.map(tool => <span class="tool"> {tool} </span>)
+        let tools = this.props.project.tools.map(tool => <li class="tool">  {tool} </li>)
         return(
             <div class="project-card">
-                <img class="project-card-image" src={this.props.project.image} alt={this.props.project.title}/>
+                <h3 class="project-title">{this.props.project.title}</h3>
                 <div class="project-card-body">
-                {/* <h3 class="project-title">{this.props.project.title}</h3> */}
-                    {/* <p class="card-text">{this.props.project.description}</p> */}
+                <img class="project-card-image" src={this.props.project.image} alt={this.props.project.title}/>
+                    <p class="card-text">{this.props.project.description}</p>
                 </div>
-                {/* {tools} */}
-             
+                <div className = "project-buttons">
+                    <a
+                      target = "_blank"
+                      rel="noopener noreferrer"
+                      class="project-button"
+                      href = {this.props.project.code}
+                      >Code</a>
+                    <a 
+                      target = "_blank"
+                      rel="noopener noreferrer"
+                      class="project-button"
+                      href={this.props.project.demo}
+                        > Demo </a>
+                      {/* <a target = "_blank" rel="noopener noreferrer" href = {this.props.project.demo}>Demo</a> */}
+                  </div>
+                  <ul className="tool-list">{tools}</ul>
             </div>
         )
     }

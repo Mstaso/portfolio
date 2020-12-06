35d5535081d38d527928a4c9b3f64db774e834b1
malcolm <mstaso1@gmail.com>
2020-12-06 16:14:48 -0500

    added styling for projects

--- portfolio/src/components/project.js
+++ portfolio/src/components/project.js
@@ -2,6 +2,21 @@ import React from 'react'
 
 
 class Project extends React.Component {
+    render() {
+        console.log(this.props)
+        let tools = this.props.project.tools.map(tool => <span class="tool"> {tool} </span>)
+        return(
+            <div class="project-card">
+                <img class="project-card-image" src={this.props.project.image} alt={this.props.project.title}/>
+                <div class="project-card-body">
+                {/* <h3 class="project-title">{this.props.project.title}</h3> */}
+                    {/* <p class="card-text">{this.props.project.description}</p> */}
+                </div>
+                {/* {tools} */}
+             
+            </div>
+        )
+    }
 
 }
 
--- portfolio/src/containers/projectcontainer.js
+++ portfolio/src/containers/projectcontainer.js
@@ -2,14 +2,15 @@ import React from 'react';
 import project_data from '../data/projects.json'
 import Project from '../components/project'
 
+
 class ProjectContainer extends React.Component {
 
     render() {
         let projects = project_data.map(project => <Project project={project} key={project.id}/>)
-        console.log(project_data)
+        // console.log(project_data)
         return (
-            <div>
-                {/* {projects} */}
+            <div class="project-section">
+                {projects}
             </div>
         )
     }
